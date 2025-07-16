fe659ca553a040a32d1ced28866c0e79a69fbf3b
permus <admin@permus.io>
2025-07-16 18:31:56 +0400

    Link Get Started Free button to login page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
-  standalone: true
+  standalone: true,
+  imports: [RouterModule]
 })
 export class LandingComponent {
 }
