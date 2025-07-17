d6f861ae26831f4a72d3a0cee5c6bff92a85dda1
permus <admin@permus.io>
2025-07-17 12:18:21 +0400

    Fix RegisterComponent errors

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ActivateAccountComponent } from './activate-account/activate-account.component';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -7,7 +12,12 @@ import { AuthRoutingModule } from './auth-routing.module';
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    ActivateAccountComponent
   ]
 })
 export class AuthModule { }
