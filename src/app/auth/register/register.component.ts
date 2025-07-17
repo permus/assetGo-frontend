471d3a27053db31af9fa248718a8e38f6663b9ce
permus <admin@permus.io>
2025-07-17 12:17:03 +0400

    Updated register.component.ts

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { OnDestroy } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
@@ -12,7 +13,6 @@ import { AuthService } from '../../core/services/auth.service';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
   registerForm: FormGroup;
   isLoading = false;
   errorMessage = '';
