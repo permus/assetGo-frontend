cbf2aec2ec20e581d371c9a2a279324a2ce1325a
permus <admin@permus.io>
2025-07-18 09:46:51 +0400

    Fix standalone component declaration in NgModule

--- src/app/locations/locations.module.ts
+++ src/app/locations/locations.module.ts
@@ -6,14 +6,13 @@ import { LocationsRoutingModule } from './locations-routing.module';
 import { LocationsComponent } from './locations.component';
 
 @NgModule({
-  declarations: [
-    LocationsComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    LocationsRoutingModule
+    LocationsRoutingModule,
+    LocationsComponent
   ]
 })
 export class LocationsModule { }
