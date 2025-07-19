7a1a054f18becce37e638ff3e6ca1723577ec837
permus <admin@permus.io>
2025-07-19 21:01:10 +0400

    Fix Router service import in standalone component

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 import { HostListener } from '@angular/core';
 import { LocationService, Location, LocationType, LocationsResponse } from './services/location.service';
@@ -13,7 +13,7 @@ import { BulkCreateModalComponent } from './components/bulk-create-modal/bulk-cr
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, AddLocationModalComponent, EditLocationModalComponent, DeleteConfirmationModalComponent, BulkCreateModalComponent],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule, AddLocationModalComponent, EditLocationModalComponent, DeleteConfirmationModalComponent, BulkCreateModalComponent],
   selector: 'app-locations',
   templateUrl: './locations.component.html',
   styleUrl: './locations.component.scss'
