b5cd21db3fc849cd9236b5b2d3afe65e6d7bfbd7
ridwancodevioso <ridwan@codevioso.com>
2025-07-24 00:28:45 +0600

    code update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://assetgo-backend.test/api',
+  apiUrl: 'https://assetgo.thethemeai.com/api',
   googleMapsApiKey: 'AIzaSyDbaa1B-gzqHyUe0iSPWUZsyI5Kl28JUHA'
 };
