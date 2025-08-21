8279359aeab56534a7814a78510a74ec66bee2e8
ridwancodevioso <ridwan@codevioso.com>
2025-08-21 12:38:03 +0600

    code update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, APP_INITIALIZER, inject } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -7,7 +6,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { FeatureFlagsService } from './core/services/feature-flags.service';
+ 
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,14 +18,6 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => {
-        const svc = inject(FeatureFlagsService);
-        return () => firstValueFrom(svc.fetchMe());
-      },
       multi: true
     }
   ]
