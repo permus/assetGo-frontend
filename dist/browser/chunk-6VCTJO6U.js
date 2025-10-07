9bef006732e958ac57fc362d8666ed4d8f4d4295
zobayer-codevioso <zobayer@codevioso.com>
2025-10-07 11:25:26 +0600

    cdoe update

--- dist/browser/chunk-6VCTJO6U.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,apiUrl:"http://assetgo-backend.test/api",googleMapsApiKey:"AIzaSyDbaa1B-gzqHyUe0iSPWUZsyI5Kl28JUHA",APP_URL:"https://portal.assetgo.equidesk.io"};export{e as a};
