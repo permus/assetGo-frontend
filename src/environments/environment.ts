7445984c107f1f11fdbbffd2a97109c881a1f5a5
Ahmed Zobaer <zobayer.me@gmail.com>
2025-11-07 18:41:19 +0600

    deployable build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://assetgo.equidesk.io/api',
-  apiUrl: 'http://assetgo-backend.test/api',
+  apiUrl: 'https://assetgo.equidesk.io/api',
+  // apiUrl: 'http://assetgo-backend.test/api',
   googleMapsApiKey: 'AIzaSyDbaa1B-gzqHyUe0iSPWUZsyI5Kl28JUHA',
   APP_URL: 'https://portal.assetgo.equidesk.io',
   PUSHER_APP_KEY: '246f4ecd3600a38d773a',
